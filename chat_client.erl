643498d7d7646909600d85e547dcefc362bb67a1
Harsh Kumar <harsh.k@greyorange.com>
2025-02-14 01:56:36 +0530

    Added functionalities such as topic creation, admin, request user and admin list for all users

--- chat_client.erl
+++ chat_client.erl
@@ -1,6 +1,13 @@
 -module(chat_client).
--export([start/2, listen/2, send_message/3, disconnect/2]).
+-export([start/2, listen/2, send_message/3,send_private_message/4, request_user_list/2, change_topic/3, make_admin/3, remove_admin/3, kick_user/3, disconnect/2]).
 
+-type username() :: string().
+-type server_pid() :: pid().
+-type message() :: string().
+-type topic() :: string().
+
+
+-spec start(server_pid(), username()) -> pid().
 start(ServerPid,Username) ->
     Pid = spawn(?MODULE, listen, [ServerPid,Username]),
     unlink(Pid),
@@ -8,15 +15,16 @@ start(ServerPid,Username) ->
     ServerPid ! {connect, Pid, Username},
     io:format("~p~n", [Pid]).
     
-
+-spec listen(server_pid(), username()) -> no_return().
 listen(Pid,Username) ->
     receive
         {msg, Message} ->
             io:format("[~p]  ~p~n", [self(),Message]),
             listen(Pid,Username);
-        %{error, Msg} ->
-         %   io:format("[~p]  Error: ~p~n", [self(),Msg]),
-          %  exit(normal);
+        {user_list, Users, Admins} ->
+            io:format("Connected Users: ~p~n", [Users]),
+            io:format("Admins: ~p~n", [Admins]),
+            listen(Pid, Username);
         {ok, WelcomeMsg} ->
             io:format("[~p]  ~p~n", [self(),WelcomeMsg]),
             {ok, Pid},
@@ -25,11 +33,34 @@ listen(Pid,Username) ->
          %   io:format("[~p]  Connection failed: ~p~n", [self(),Reason]),
           %  exit(normal)
     end.
-
+-spec send_message(username(), server_pid(), message()) -> no_return().
 send_message(Username,ServerPid, Msg) ->
     ServerPid ! {message, Username, Msg}.
 
+send_private_message(Sender, ServerPid, Recipient, Msg) ->
+    ServerPid ! {private_message, Sender, Recipient, Msg}.
 
+request_user_list(Username, ServerPid) ->
+    ServerPid ! {request_user_list, Username}.
+
+-spec change_topic(username(), server_pid(), topic()) -> no_return().
+change_topic(Username, ServerPid, NewTopic) ->
+    ServerPid ! {change_topic, Username, NewTopic}.
+
+-spec make_admin(username(), server_pid(), username()) -> no_return().
+make_admin(AdminUsername, ServerPid, TargetUsername) ->
+    ServerPid ! {make_admin, AdminUsername, TargetUsername}.
+
+-spec remove_admin(username(), server_pid(), username()) -> no_return().
+remove_admin(AdminUsername, ServerPid, TargetUsername) ->
+    ServerPid ! {remove_admin, AdminUsername, TargetUsername}.
+
+-spec kick_user(username(), server_pid(), username()) -> no_return().
+kick_user(AdminUsername, ServerPid, TargetUsername) ->
+    ServerPid ! {kick, AdminUsername, TargetUsername}.
+
+
+-spec disconnect(username(), server_pid()) -> no_return().
 disconnect(Username,ServerPid) ->
     ServerPid ! {disconnect, Username},
     exit(self(), normal).
--- chat_server.erl
+++ chat_server.erl
@@ -1,27 +1,42 @@
 -module(chat_server).
--export([start/1, loop/2]).
+-export([start/1, loop/4]).
 
+-type username() :: string().
+-type server_pid() :: pid().
+-type user_list() :: [{username(), server_pid()}].
+-type max_users() :: pos_integer().
+-type topic() :: string().
+-type admin_list() :: [username()].
+-type message() :: string().
+
+
+-spec start(max_users()) -> server_pid().
 start(MaxUsers) when is_integer(MaxUsers), MaxUsers > 0 ->
-    Pid = spawn(?MODULE, loop, [[],MaxUsers]),
+    Pid = spawn(?MODULE, loop, [[],MaxUsers,"General",[]]),
     unlink(Pid),
     Pid.
 
-
-loop(Users,MaxUsers) ->
+-spec loop(user_list(), max_users(), topic(), admin_list()) -> no_return().
+loop(Users,MaxUsers,CurrentTopic,Admins) ->
     receive
         {connect, Pid, Username} ->
             case lists:keyfind(Username, 1, Users) of
                 false when length(Users) < MaxUsers ->
                     Pid ! {ok, "Welcome, " ++ Username},
                     NewUsers = [{Username, Pid} | Users],
+                    NewAdmins = case length(Admins) of
+                        0 -> [Username];  
+                        _ -> Admins
+                    end,
                     broadcast(NewUsers, Username ++ " has joined the chat."),
-                    loop(NewUsers,MaxUsers);
+                    loop(NewUsers,MaxUsers,CurrentTopic,NewAdmins);
                 false ->
                     Pid ! {error, "Server full, max" ++ integer_to_list(MaxUsers) ++ "users allowed."},
-                    loop(Users,MaxUsers);
+                    loop(Users,MaxUsers,CurrentTopic,Admins);
                 _ ->
                     Pid ! {error, "Username already taken."},
-                    loop(Users,MaxUsers)
+                    broadcast(Users,"Username " ++  Username ++ " already taken."),
+                    loop(Users,MaxUsers,CurrentTopic,Admins)
             end;
         
         {message, Username, Msg} ->
@@ -29,15 +44,71 @@ loop(Users,MaxUsers) ->
             FormattedTimestamp = io_lib:format("~p", [Timestamp]),
             Flatenned = lists:flatten(FormattedTimestamp),
             broadcast(Users, "[" ++ Username ++ " - " ++ Flatenned ++ "] " ++ Msg),
-            loop(Users,MaxUsers);
+            loop(Users,MaxUsers,CurrentTopic,Admins);
 
-    
+        {private_message, Sender, Recipient, Msg} ->
+            case lists:keyfind(Recipient, 1, Users) of
+                {Recipient, Pid} ->
+                    Pid ! {msg, "[Private from " ++ Sender ++ "] " ++ Msg},
+                    loop(Users, MaxUsers, CurrentTopic, Admins);
+                false ->
+                    lists:keyfind(Sender, 1, Users) ! {error, "User " ++ Recipient ++ " not found."},
+                    loop(Users, MaxUsers, CurrentTopic, Admins)
+            end;
+
+        {request_user_list, Username} ->
+            case lists:keyfind(Username, 1, Users) of
+                {Username, Pid} ->
+                    Usernames = [U || {U, _} <- Users],
+                    Pid ! {user_list, Usernames, Admins},
+                    loop(Users, MaxUsers, CurrentTopic, Admins);
+                false ->
+                    loop(Users, MaxUsers, CurrentTopic, Admins)
+            end;
+
+        {change_topic, Username, NewTopic} ->
+            broadcast(Users, Username ++ " changed the topic to: " ++ NewTopic),
+            loop(Users, MaxUsers, NewTopic,Admins);
+
+        {make_admin, AdminUsername, TargetUsername} ->
+            case lists:member(AdminUsername, Admins) of
+                true ->
+                    NewAdmins = lists:usort([TargetUsername | Admins]),
+                    broadcast(Users, TargetUsername ++ " has been made an admin by " ++ AdminUsername),
+                    loop(Users, MaxUsers, CurrentTopic, NewAdmins);
+                false ->
+                    broadcast(Users, AdminUsername ++ " is not an admin "),
+                    loop(Users, MaxUsers, CurrentTopic, Admins)
+            end;
+
+        {remove_admin, AdminUsername, TargetUsername} ->
+            case lists:member(AdminUsername, Admins) of
+                true ->
+                    NewAdmins = lists:delete(TargetUsername, Admins),
+                    broadcast(Users, TargetUsername ++ " is no longer an admin."),
+                    loop(Users, MaxUsers, CurrentTopic, NewAdmins);
+                false ->
+                    broadcast(Users, AdminUsername ++ " is not an admin "),
+                    loop(Users, MaxUsers, CurrentTopic, Admins)
+            end;
+
+        {kick, AdminUsername, TargetUsername} ->
+            case lists:member(AdminUsername, Admins) of
+                true ->
+                    NewUsers = lists:keydelete(TargetUsername, 1, Users),
+                    broadcast(NewUsers, TargetUsername ++ " has been kicked out by " ++ AdminUsername),
+                    loop(NewUsers, MaxUsers, CurrentTopic, lists:delete(TargetUsername, Admins));
+                false ->
+                    broadcast(Users, AdminUsername ++ " is not an admin "),
+                    loop(Users, MaxUsers, CurrentTopic, Admins)
+            end;
 
         {disconnect, Username} ->
             NewUsers = lists:keydelete(Username, 1, Users),
             broadcast(NewUsers, Username ++ " has left the chat."),
-            loop(NewUsers,MaxUsers)
+            loop(NewUsers,MaxUsers,CurrentTopic,Admins)
     end.
 
+-spec broadcast(user_list(), message()) -> ok.
 broadcast(Users, Message) ->
     lists:foreach(fun({_, Pid}) -> Pid ! {msg, Message} end, Users).
