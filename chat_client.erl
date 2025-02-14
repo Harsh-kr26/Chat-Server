e7188cfd644a8dd7c1f6efd50ddb96c4281498bb
Harsh Kumar <harsh.k@greyorange.com>
2025-02-14 14:32:12 +0530

    Added function specs in both server-client code.

--- chat_client.erl
+++ chat_client.erl
@@ -33,34 +33,36 @@ listen(Pid,Username) ->
          %   io:format("[~p]  Connection failed: ~p~n", [self(),Reason]),
           %  exit(normal)
     end.
--spec send_message(username(), server_pid(), message()) -> no_return().
+-spec send_message(username(), server_pid(), message()) -> {message, username(), message()}.
 send_message(Username,ServerPid, Msg) ->
     ServerPid ! {message, Username, Msg}.
 
+-spec send_private_message(username(), server_pid(), username(),message()) -> {private_message, username(), username(), message()}.
 send_private_message(Sender, ServerPid, Recipient, Msg) ->
     ServerPid ! {private_message, Sender, Recipient, Msg}.
 
+-spec request_user_list(username(), server_pid()) -> {request_user_list, username()}.
 request_user_list(Username, ServerPid) ->
     ServerPid ! {request_user_list, Username}.
 
--spec change_topic(username(), server_pid(), topic()) -> no_return().
+-spec change_topic(username(), server_pid(), topic()) -> {change_topic, username(), topic()}.
 change_topic(Username, ServerPid, NewTopic) ->
     ServerPid ! {change_topic, Username, NewTopic}.
 
--spec make_admin(username(), server_pid(), username()) -> no_return().
+-spec make_admin(username(), server_pid(), username()) -> {make_admin, username(), username()}.
 make_admin(AdminUsername, ServerPid, TargetUsername) ->
     ServerPid ! {make_admin, AdminUsername, TargetUsername}.
 
--spec remove_admin(username(), server_pid(), username()) -> no_return().
+-spec remove_admin(username(), server_pid(), username()) -> {remove_admin, username(), username()}.
 remove_admin(AdminUsername, ServerPid, TargetUsername) ->
     ServerPid ! {remove_admin, AdminUsername, TargetUsername}.
 
--spec kick_user(username(), server_pid(), username()) -> no_return().
+-spec kick_user(username(), server_pid(), username()) -> {kick, username(), username()}.
 kick_user(AdminUsername, ServerPid, TargetUsername) ->
     ServerPid ! {kick, AdminUsername, TargetUsername}.
 
 
--spec disconnect(username(), server_pid()) -> no_return().
+-spec disconnect(username(), server_pid()) -> {disconnect, username()}.
 disconnect(Username,ServerPid) ->
     ServerPid ! {disconnect, Username},
     exit(self(), normal).
